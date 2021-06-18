29b4712053dce2733ab01e3b75a4b206e9ef1717
Julio <julioarmando321@gmail.com>
2021-06-18 11:29:35 -0300

    feat: add screens and stacknavigator

--- App.js
+++ App.js
@@ -1,21 +1,87 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+
+import {
+	SignIn,
+	CreateAccount,
+	Search,
+	Home,
+	Details,
+	Search2,
+	Profile,
+} from './Screens';
+
+const AuthStack = createStackNavigator();
+const Tabs = createBottomTabNavigator();
+
+const HomeStack = createStackNavigator();
+const SearchStack = createStackNavigator();
+
+const HomeStackScreen = () => (
+	<HomeStack.Navigator>
+		<HomeStack.Screen name='Home' component={Home} />
+		<HomeStack.Screen
+			name='Details'
+			component={Details}
+			options={({ route }) => ({
+				title: route.params.name,
+			})}
+		/>
+	</HomeStack.Navigator>
+);
+
+const SearchStackScreen = () => (
+	<SearchStack.Navigator>
+		<SearchStack.Screen name='Search' component={Search} />
+		<SearchStack.Screen name='Search2' component={Search2} />
+	</SearchStack.Navigator>
+);
+
+const ProfileStack = createStackNavigator();
+const ProfileStackScreen = () => (
+	<ProfileStack.Navigator>
+		<ProfileStack.Screen name='Profile' component={Profile} />
+	</ProfileStack.Navigator>
+);
+
+const TabsScreen = () => (
+	<Tabs.Navigator>
+		<Tabs.Screen name='Home' component={HomeStackScreen} />
+		<Tabs.Screen name='Search' component={SearchStackScreen} />
+	</Tabs.Navigator>
+);
+
+const Drawer = createDrawerNavigator();
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<NavigationContainer>
+			<Drawer.Navigator>
+				<Drawer.Screen name='Home' component={TabsScreen} />
+				<Drawer.Screen name='Profile' component={ProfileStackScreen} />
+			</Drawer.Navigator>
+			{/* <AuthStack.Navigator>
+				<AuthStack.Screen
+					name='SignIn'
+					component={SignIn}
+					options={{ title: 'Sign In' }}
+				/>
+				<AuthStack.Screen
+					name='CreateAccount'
+					component={CreateAccount}
+					options={{ title: 'Create Account' }}
+				/>
+			</AuthStack.Navigator> */}
+		</NavigationContainer>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {},
 });
